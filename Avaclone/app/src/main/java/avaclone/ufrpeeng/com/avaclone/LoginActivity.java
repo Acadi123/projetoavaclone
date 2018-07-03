e7ca41766f8dfcb9bf023192d6acf7afbb48b2f4
Dimitri Medeiros Marinovic <dimitrimarinovic@gmail.com>
2018-07-03 10:37:59 -0300

    Android Projeto Dimi
    Android Projeto Dimi

---
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/LoginActivity.java
@@ -0,0 +1,21 @@
+package avaclone.ufrpeeng.com.avaclone;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText
+    private EditText ;
+    private Button
+    private boolean
+    private SharedPreferences
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
