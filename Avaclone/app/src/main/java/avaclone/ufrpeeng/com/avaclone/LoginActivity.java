e564eb7e90d85dadcaa6b71bc1e6facd552777ae
Dimitri Medeiros Marinovic <dimitrimarinovic@gmail.com>
2018-07-03 22:44:30 -0300

    teste

--- Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/LoginActivity.java
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/LoginActivity.java
@@ -4,15 +4,18 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText
-    private EditText ;
-    private Button
-    private boolean
-    private SharedPreferences
+    private EditText LoginTxtInput;
+    private EditText PassTxtInput;
+    private Button BtnLogin;
+    private CheckBox RememberBox;
+    private boolean LoggedIn = false;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
