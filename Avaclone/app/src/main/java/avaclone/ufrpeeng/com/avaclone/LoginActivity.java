9bca7931c663c1a5c88b22a307f1c58ee3c62e5f
Dimitri Medeiros Marinovic <dimitrimarinovic@gmail.com>
2018-07-12 01:47:30 -0300

    Login

--- Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/LoginActivity.java
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/LoginActivity.java
@@ -1,8 +1,10 @@
 package avaclone.ufrpeeng.com.avaclone;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
@@ -12,79 +14,85 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.URL;
-import java.net.HttpURLConnection;
-import java.util.Scanner;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         Button buttonlogin = findViewById(R.id.BtnLogin);
         buttonlogin.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                try {
-                    Logar();
-                }
 
-                catch (Exception ex) {
-                    Toast toast = Toast.makeText(getApplicationContext(), "Usuario ou senha errado(s)", Toast.LENGTH_SHORT);
-                    toast.show();
+                Returnrequest
 
-                }
+                OkHttpClient okHttpClient;
+                Request request;
+
+                EditText LoginTxt, PassTxt;
+                String Login, Senha;
+
+                LoginTxt = findViewById(R.id.LoginTxtInput);
+                PassTxt = findViewById(R.id.PassTxtInput);
+
+                okHttpClient = new OkHttpClient();
+
+                Login = LoginTxt.getText().toString();
+                Senha = PassTxt.getText().toString();
+
+                HttpUrl.Builder urlBuilder = HttpUrl.parse("http://ava.ufrpe.br/login/token.php").newBuilder();
+                urlBuilder.addQueryParameter("username", Login);
+                urlBuilder.addQueryParameter("password", Senha);
+                urlBuilder.addQueryParameter("service", "moodle_mobile_app");
+                String url = urlBuilder.build().toString();
+
+                request = new Request.Builder().url(url).build();
+
+                okHttpClient.newCall(request).enqueue(new Callback() {
+
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        returnRequest.retrieveData(null);
+                        //TOAST COM PROBLEMA DE CONEXAO E REINICIA A PAGINA
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        String result = response.body().string();
+                        try {
+                            JSONObject jsonObj = new JSONObject(result);
+                            String token = jsonObj.getString("token");
+                            returnRequest.retrieveData(token);
+                            //INTENT PARA A PROXIMA PAGINA AQUI E REINICIA A PAGINA E GUARDA O TOKEN
+                            //PARA PROXIMA PAGINA
+                        } catch (Exception e) {
+                            returnRequest.retrieveData(null);
+                            //TOAST AQUI
+                        }
+                    }
+                });
+            }
+
+            public void Trocarpagina() {
+                Intent it = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(it);
             }
         });
     }
-
-    private  void  Logar() throws IOException, JSONException {
-
-        EditText LoginTxt, PassTxt;
-        String Login, Senha;
-
-        LoginTxt = findViewById(R.id.LoginTxtInput);
-        PassTxt = findViewById(R.id.PassTxtInput);
-        Login = LoginTxt.getText().toString();
-        Senha = PassTxt.getText().toString();
-        String tokenout = "";
-
-        JSONObject jsonAVA = new JSONObject();
-        jsonAVA.put("username", Login);
-        jsonAVA.put("password", Senha);
-        jsonAVA.put("service", "moodle_mobile_app");
-
-        URL url = new URL("http://ava.ufrpe.br/login/token.php");
-
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
-        conn.setRequestProperty("Content-type", "application/json");
-        conn.setRequestProperty("Accept", "application/json");
-        conn.connect();
-
-        PrintStream printStream = new PrintStream(conn.getOutputStream());
-        printStream.println(jsonAVA);
-
-        String jsonResposta = new Scanner(conn.getInputStream()).next();
-
-        conn.disconnect();
-
-        tokenout=jsonResposta;
-
-        //IF O TOKEN FOR RECEBIDO -> Trocar a pagina()
-
-        if (!tokenout.equals("")) Trocarpagina();
-    }
-
-    public void Trocarpagina(){
-        Intent it = new Intent(LoginActivity.this, CadastroActivity.class);
-        startActivity(it);
-    }
 }
+//                    Toast toast = Toast.makeText(getApplicationContext(), "Usuario ou senha errado(s)", Toast.LENGTH_SHORT);
+//                    toast.show();
