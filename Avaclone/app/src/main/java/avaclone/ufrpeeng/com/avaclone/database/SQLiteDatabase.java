4f4e71df0470f738aa37b00cee8f70674b960ca7
Acadi <ac.adi@hotmail.com>
2018-07-17 10:36:51 -0300

    atualizaçao
    banco "SQLitedatabase"

--- Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/database/SQLiteDatabase.java
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/database/SQLiteDatabase.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class dadosopenhelpr extends SQLiteOpenHelper {
+class dadosopenhelpr extends SQLiteOpenHelper {
 
+
+    public dadosopenhelpr(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
