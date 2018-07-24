70720f1e9cad7f275f8da99601b0550ac148b112
Dimitrimm <40797429+Dimitrimm@users.noreply.github.com>
2018-07-24 03:28:25 -0300

    Pagina perfil funcional. Apresentar em sala.
    Consegui consumir a api de dados do usuario(2).
    Não vou comparecer na aula mas no mesmo dia vou ter a pagina de disciplinas feita assim como o menu.
    Mostra pra Cleviton que o perfil e o login estão funcionando e diz pra ele ficar de olho no GIT amanhã que até o final do dia eu vou por a pagina dee disciplinas funcionando e o menu também.

--- Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/CadastroActivity.java
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/CadastroActivity.java
@@ -3,11 +3,13 @@ package avaclone.ufrpeeng.com.avaclone;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,7 +23,10 @@ import okhttp3.Response;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    TextView nometxt,cpftxt,cursotxt,emailtxt,cidadetxt;
+    String parametros1 = "";
+    String parametros2 = "";
+    String userid = "";
+    TextView nometxt, cpftxt, cursotxt, emailtxt, cidadetxt, univtxt;
 
     String url = "http://ava.ufrpe.br/webservice/rest/server.php?moodlewsrestformat=json";
 
@@ -36,64 +41,99 @@ public class CadastroActivity extends AppCompatActivity {
         if (extras != null) {
             String token = extras.getString("token");
 
-            String parametros1 = "wsfunction=" + "core_webservice_get_site_info" + "&wstoken=" + token;
+            parametros1 = "wsfunction=" + "core_webservice_get_site_info" + "&wstoken=" + token;
+            parametros2 = "wsfunction=" + "core_user_get_users_by_id" + "&wstoken=" + token;
 
             ConnectivityManager connMgr = (ConnectivityManager)
                     getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
             if (networkInfo != null && networkInfo.isConnected()) {
-
-                String resposta1 = Requisitor.recolhedados(url, parametros1);
-
-
-                if (resposta1 != null) {
-
-                    try {
-                        JSONObject retorno1 = new JSONObject(resposta1);
-                        String id = retorno1.getString("id");
-                        String parametros2 = "wsfunction=" + "core_user_get_users_by_id" + "&wstoken=" + token + "&userids[0]=" + id;
-
-                        String resposta2 = Requisitor.recolhedados(url, parametros2);
-
-                        JSONObject retorno2 = new JSONObject(resposta2);
-                        String Nome = retorno2.getString("fullname");
-                        String CPF = retorno2.getString("idnumber");
-                        String Curso = retorno2.getString("department");
-                        String Email = retorno2.getString("email");
-                        String Cidade = retorno2.getString("city");
-
-                        nometxt.setText(Nome);
-                        cpftxt.setText(CPF);
-                        cursotxt.setText(Curso);
-                        emailtxt.setText(Email);
-                        cidadetxt.setText(Cidade);
-
-
-
-                    } catch (JSONException e) {
-                        erro.show();
-                    }
-
-                }
-                else {
-                    erro.show();
-                }
+                new SolocitaDados1().execute(url);
             }
             else {
                 erro.show();
             }
         }
-        else {
+         else {
             erro.show();
         }
     }
 
-    public static class Requisitor{
+
+    public class SolocitaDados1 extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            return Conexao.postDados(urls[0], parametros1);
+        }
+
+        protected void onPostExecute(JSONObject resultado) {
+            Toast erro = Toast.makeText(getApplicationContext(), "Algo deu errado.", Toast.LENGTH_LONG);
+            if (resultado != null) try {
+                userid = resultado.getString("userid");
+                parametros2 = parametros2 + "&userids[0]=" + userid;
+            } catch (JSONException e) {
+                erro.show();
+            }
+            else {
+                erro.show();
+            }
+            new SolocitaDados2().execute(url);
+        }
+    }
 
 
-        public static String recolhedados(String urlUsuario, String parametrosUsuario) {
+    public class SolocitaDados2 extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            return Conexao2.postDados(urls[0], parametros2);
+        }
 
+        protected void onPostExecute(JSONObject resultado) {
+            Toast erro = Toast.makeText(getApplicationContext(), "Algo deu errado.", Toast.LENGTH_LONG);
+
+            if (resultado != null) {
+                try {
+
+                    String nome = resultado.getString("fullname");
+                    String cpf = resultado.getString("idnumber");
+                    String curso = resultado.getString("department");
+                    String email = resultado.getString("email");
+                    String cidade = resultado.getString("city");
+                    String univ = resultado.getString("institution");
+
+                    nometxt = findViewById(R.id.NomeSet);
+                    cpftxt = findViewById(R.id.CpfSet);
+                    cursotxt = findViewById(R.id.CursoSet);
+                    emailtxt = findViewById(R.id.EmailSet);
+                    cidadetxt = findViewById(R.id.CidadeSet);
+                    univtxt = findViewById(R.id.UnivSet);
+
+                    nometxt.setText(nome);
+                    cpftxt.setText(cpf);
+                    cursotxt.setText(curso);
+                    emailtxt.setText(email);
+                    cidadetxt.setText(cidade);
+                    univtxt.setText(univ);
+
+                    Toast sucesso = Toast.makeText(getApplicationContext(), "Informações de perfil carregadas.", Toast.LENGTH_LONG);
+                    sucesso.show();
+
+
+
+                } catch (JSONException e) {
+                    erro.show();
+                }
+            }
+            else {
+                erro.show();
+                }
+            }
+        }
+
+    public static class Conexao {
+
+        public static JSONObject postDados(String urlUsuario, String parametrosUsuario) {
             OkHttpClient client = new OkHttpClient();
 
             Request.Builder builder1 = new Request.Builder();
@@ -106,14 +146,50 @@ public class CadastroActivity extends AppCompatActivity {
             builder1.post(body1);
 
             Request request1 = builder1.build();
-                try {
-                    Response response = client.newCall(request1).execute();
-                    String result = response.body().string();
-                    return result;
-                } catch (IOException e) {
-                    return null;
-                }
+
+            try {
+                Response response = client.newCall(request1).execute();
+                String result = response.body().string();
+                JSONObject jsonobj = new JSONObject(result);
+                return jsonobj;
+            } catch (IOException e) {
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+            return null;
+        }
+    }
+    public static class Conexao2 {
+
+        public static JSONObject postDados(String urlUsuario, String parametrosUsuario) {
+            OkHttpClient client = new OkHttpClient();
+
+            Request.Builder builder1 = new Request.Builder();
+
+            builder1.url(urlUsuario);
+
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+
+            RequestBody body1 = RequestBody.create(mediaType, parametrosUsuario);
+            builder1.post(body1);
+
+            Request request1 = builder1.build();
+
+            try {
+                Response response = client.newCall(request1).execute();
+                String result = response.body().string();
+                JSONArray jsonArray = new JSONArray(result); ///PROBLEMA AQUI JSONARRAY
+                JSONObject jsonobj;
+                jsonobj = jsonArray.getJSONObject(0);
+                return jsonobj;
+            } catch (IOException e) {
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
 
