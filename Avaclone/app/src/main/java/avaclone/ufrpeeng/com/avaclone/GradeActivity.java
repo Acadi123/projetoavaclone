65317d0316f47012ec453225f5fab8ff7f603dff
Acadi <ac.adi@hotmail.com>
2018-07-26 22:40:56 -0300

    tela grade
    atualização da tela grade

---
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/GradeActivity.java
@@ -0,0 +1,9 @@
+package avaclone.ufrpeeng.com.avaclone;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class GradeActivity extends AppCompatActivity{
+
+    String url = "http://ava.ufrpe.br/webservice/rest/server.php?moodlewsrestformat=json";
+}
