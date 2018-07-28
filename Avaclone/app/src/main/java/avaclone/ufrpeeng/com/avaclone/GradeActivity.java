ea8a1b4f5bf5d0e3bfda1304a9b15df42a0c6ee2
Acadi <ac.adi@hotmail.com>
2018-07-27 22:41:49 -0300

    Modificação da tela grade

--- Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/GradeActivity.java
+++ Avaclone/app/src/main/java/avaclone/ufrpeeng/com/avaclone/GradeActivity.java
@@ -1,9 +1,32 @@
 package avaclone.ufrpeeng.com.avaclone;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 
-public class GradeActivity extends AppCompatActivity{
+public class GradeActivity extends AppCompatActivity {
+
+    String token;
 
     String url = "http://ava.ufrpe.br/webservice/rest/server.php?moodlewsrestformat=json";
+
+    ListView listgradeW;
+
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_grade);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listgradeW = findViewById(R.id.listgrade);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
 }
